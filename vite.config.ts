1c2e2ebbcc83297d872d2d1592bc78a370afdfab
Utkarsh <utkarshp@Ninja-ITs-MacBook-Air.local>
2025-01-18 15:54:25 +0530

    base path resolved

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/simon-game-vite/', // Set the base path to the GitHub Pages subdirectory
   plugins: [react()],
 })
